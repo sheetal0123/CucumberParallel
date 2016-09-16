5a7b6dceea11140a0405b41975eb0138fa518dfc
Nitin <ngoyal@xebia.com>
2016-09-16 10:29:52 +0530

    trying one more time

--- src/test/java/Utility/DriverFactory.java
+++ src/test/java/Utility/DriverFactory.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DriverFactory {
@@ -16,12 +17,20 @@ public class DriverFactory {
 	public void createdriver()
 	{
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ File.separator + "src" + File.separator + "main"  + File.separator + "resources" + File.separator + "drivers" + File.separator + "chromedriver.exe");
-		/*ChromeOptions options=new ChromeOptions();
-		options.addArguments("no-sandbox");
+		ChromeOptions options = new ChromeOptions();
 		options.addArguments("test-type");
 		options.addArguments("start-maximized");
-		options.addArguments("--disable-extensions");*/
-		driver=new ChromeDriver();
+		options.addArguments("--disable-extensions");
+		options.addArguments("no-sandbox");
+
+		options.addArguments("ignore-certificate-errors");
+		options.addArguments("--allow-running-insecure-content");
+
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		//capabilities.setCapability("chrome.binary", WINDOWS_CHROME_DRIVER);
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+		driver=new ChromeDriver(options);
 	    driver.manage().window().maximize();
 	    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	    driver.get("https://www.flipkart.com");
